038c8a916c2abda112322e4929aa50825db392eb
nurulla-hasan <nurullahasan.dev@gmail.com>
2026-03-10 16:53:18 +0600

    style(vendor-layout): remove md:p-6 padding from main content area

--- src/components/vendor/layout/main-layout.tsx
+++ src/components/vendor/layout/main-layout.tsx
@@ -22,7 +22,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
         
         <main className="flex-1 overflow-hidden pt-20">
           <ScrollArea className="h-full">
-            <div className="p-4 md:p-6">
+            <div className="p-4">
               {children}
             </div>
           </ScrollArea>
